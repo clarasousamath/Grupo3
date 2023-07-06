1f0bf628fc5bc5ddc839305a2ff73d8ef50b46ab
Mirraely <mirraelymoura@gmail.com>
2023-07-05 22:58:36 -0300

    Reimplementa a animação do carrossel

--- scripts/carrossel.js
+++ scripts/carrossel.js
@@ -1,34 +1,48 @@
+// Informações relacionadas às imagens do carrossel
 const imagens = [
     {
         endereco: './imagens/index/tela-inicial-1.jpg',
         texto: 'Técnicas de meditação',
-        descricaoImagem: 'Imagem 1 da tela inicial do carrossel. Imagem com uma mulher meditando em cima de um tapete rosa, em posição de ioga, vestindo uma calça jeans azul e uma blusa branca, e com cabelos amarrados, com gato dormindo ao lado esquerdo da mulher. Ao fundo, várias plantas e flores em jarros. Uma caneca vermelha com desenho de coração branco ao lado direito da mulher. Ao fundo da imagem, uma parede de cor verde.'
+        descricao: 'Imagem 1 da tela inicial do carrossel. Imagem com uma mulher meditando em cima de um tapete rosa, em posição de ioga, vestindo uma calça jeans azul e uma blusa branca, e com cabelos amarrados, com gato dormindo ao lado esquerdo da mulher. Ao fundo, várias plantas e flores em jarros. Uma caneca vermelha com desenho de coração branco ao lado direito da mulher. Ao fundo da imagem, uma parede de cor verde.'
     },
     {
         endereco: './imagens/index/tela-inicial-2.jpg',
         texto: 'Hábitos saudáveis',
-        descricaoImagem: 'Imagem 2 da tela inicial do carrossel. Imagem de homem meditando em cima de uma mesa, em posição de ioga, vestindo uma calça azul, camisa social branca e gravata vermelha. Ao lado direito do homem, um jarro azul com planta, e ao lado esquerdo, uma caneca vermelha com uma bebida quente. Por trás do homem tem um céu em camadas azúis, cheio de nuvens. Em volta do homem, objetos flutuando (bola colorida, pasta de arquivos amarela, lâmpada amarela e ficha de papel)'
+        descricao: 'Imagem 2 da tela inicial do carrossel. Imagem de homem meditando em cima de uma mesa, em posição de ioga, vestindo uma calça azul, camisa social branca e gravata vermelha. Ao lado direito do homem, um jarro azul com planta, e ao lado esquerdo, uma caneca vermelha com uma bebida quente. Por trás do homem tem um céu em camadas azúis, cheio de nuvens. Em volta do homem, objetos flutuando (bola colorida, pasta de arquivos amarela, lâmpada amarela e ficha de papel)'
     },
     {
         endereco: './imagens/index/tela-inicial-3.jpg',
         texto: 'Receba ajuda',
-        descricaoImagem: 'Imagem 3 da tela inicial do carrossel. Imagem de família em um jardim, com uma casa azul e uma cerca ao fundo da imagem e núvens e pássaros no céu. Mulher vestindo blusa vermelha, calça azul e descalça, fazendo o número quatro com as pernas e gesto de agradecimento com as mãos. Menina ao lado direito da mulher, em cima do tapete, vestindo uma blusa rosa clara e azul clara, também descalça, e espelhando a posição da mulher. Ao lado esquerdo da mulher, tem um menino vestindo camisa azul e calça vermelha, usando sapato azul, sentado na grama, brincando com cachorro branco com manchas caramelo. E nos dois lados da imagem, temos duas plantas e uma libélula.'
+        descricao: 'Imagem 3 da tela inicial do carrossel. Imagem de família em um jardim, com uma casa azul e uma cerca ao fundo da imagem e núvens e pássaros no céu. Mulher vestindo blusa vermelha, calça azul e descalça, fazendo o número quatro com as pernas e gesto de agradecimento com as mãos. Menina ao lado direito da mulher, em cima do tapete, vestindo uma blusa rosa clara e azul clara, também descalça, e espelhando a posição da mulher. Ao lado esquerdo da mulher, tem um menino vestindo camisa azul e calça vermelha, usando sapato azul, sentado na grama, brincando com cachorro branco com manchas caramelo. E nos dois lados da imagem, temos duas plantas e uma libélula.'
     }
 ];
 
+// Recuperando na DOM o conteiner do carrossel
 const conteinerCarrossel = document.querySelector('.conteiner-carrossel');
+const carrossel = document.querySelector(".carrossel");
 
+// Renderizando o carrossel
 conteinerCarrossel.innerHTML = criarCarrossel(imagens);
 
+/**
+ * Renderiza individualmente uma imagem do carrossel
+ * @param {{endereco: string, texto: string, descricao: string}} imagem 
+ * @returns retorna uma string do html necessário para uma imagem do carrossel
+ */
 function criarImagem(imagem) {
     return `
         <figure>
-            <img src="${imagem.endereco}" alt="${imagem.descricaoImagem}">
+            <img src="${imagem.endereco}" alt="${imagem.descricao}">
             <figcaption>${imagem.texto}</figcaption>
         </figure> 
     `;
 }
 
+/**
+ * Renderiza completamente o carrossel com as suas imagens
+ * @param {[{endereco: string, texto: string, descricao: string}]} imagens 
+ * @returns retorna uma string do html necessário para o carrossel
+ */
 function criarCarrossel(imagens) {
     let listaImagens = "";
     let numero = 1;
@@ -44,75 +58,47 @@ function criarCarrossel(imagens) {
         numero += 1;
     }
 
+    let navegacao = "";
+    for (var i = 1; i <= imagens.length; i++) {
+        navegacao += `<a href="#imagem${i}">&#9679;</a>`
+    }
+
     return `
         <div class="carrossel">
             ${listaImagens}
         </div>
         <div class="navegacao-carrossel">
-            <a href="#imagem1">&#9679;</a>
-            <a href="#imagem2">&#9679;</a>
-            <a href="#imagem3">&#9679;</a>
+           ${navegacao}
         </div>
     `
 }
 
-/* Inserindo animação do carrossel com função nativa do js*/
+/* Inserindo animação do carrossel com função nativa do js */
 let numero = 1;
+let pausado = false;
 
-// setInterval(() => {
-//     const imagemAtual = document.getElementById(`imagem${numero}`);
-//     const proximoNumero = (numero >= imagens.length ? 1 : numero + 1);
-//     const proximaImagem = document.getElementById(`imagem${proximoNumero}`);
+/* Código responsável pela animação do carrossel */
+setInterval(() => {
+    if (!pausado) {
+        const imagem = document.getElementById(`imagem${numero}`);
+        carrossel.scroll({
+            left: imagem.getBoundingClientRect().width * (numero - 1),
+            behavior: 'smooth'
+        });
 
-//     if (operacao === "sumir") {
-//         imagemAtual.className = "";
-//         operacao = "";
-//         numero = proximoNumero;
-//     } else {
-//         if (operacao === "aparecer") {
-//             operacao = "ficar"
-//         } else if (operacao === "ficar") {
-//             operacao = "sumir"
-//         } else {
-//             operacao = "aparecer";
-//         }
+        numero = (numero >= imagens.length ? 1 : numero + 1);
+    }
 
-//         imagemAtual.classList.add(operacao);
-//     }
-// }, 1000);
+}, 2500);
 
-// function recuperarImagem(numero) {
-//     const imagem = document.getElementById(`imagem${numero}`);
+/* Código responsável por pausar a animação quando o mouse fica em cima do carrossel */
+carrossel.addEventListener("mouseenter", () => {
+    pausado = true;
+});
 
-//     return imagem;
-// }
+/* Código responsável por retomar a animação quando o mouse sai de cima do carrossel */
+carrossel.addEventListener("mouseleave", () => {
+    pausado = false;
+});
 
-// const imagem1 = recuperarImagem(1);
-// animarImagem(imagem1);
-
-// function animarImagem(imagemAtual) {
-//     let operacao = "";
-
-//     const id = setInterval(() => {
-//         const proximoNumero = (numero >= imagens.length ? 1 : numero + 1);
-//         const proximaImagem = recuperarImagem(proximoNumero);
-
-//         if (operacao === "sumir") {
-//             imagemAtual.className = "";
-//             operacao = "";
-//             numero = proximoNumero;
-//             clearInterval(id);
-//         } else {
-//             if (operacao === "aparecer") {
-//                 operacao = "ficar"
-//                 animarImagem(proximaImagem);
-//             } else if (operacao === "ficar") {
-//                 operacao = "sumir";
-//             } else {
-//                 operacao = "aparecer";
-//             }
-//             imagemAtual.classList.add(operacao);
-//         }
-//     }, 1000);
-// }
 
