82686843ffdd5f8cbd9120c555166131908f1238
Mirraely <mirraelymoura@gmail.com>
2023-07-19 23:06:13 -0300

    Padroniza palavras reservadas

--- docs/raizes_cura.sql
+++ docs/raizes_cura.sql
@@ -1,28 +1,28 @@
-create database raizes_cura;
+CREATE DATABASE raizes_cura;
 
 \c raizes_cura;
 
-create table usuario (
+CREATE TABLE usuario (
     email VARCHAR(128) NOT NULL PRIMARY KEY,
     senha VARCHAR(256) NOT NULL,
     perfil VARCHAR(50) NOT NULL,
     bio TEXT NOT NULL,
-    data_cadastro TIMESTAMP NOT NULL default current_timestamp
+    data_cadastro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-create table artigo (
+CREATE TABLE artigo (
     titulo VARCHAR(128) NOT NULL PRIMARY KEY,
     subtitulo VARCHAR(256),
-    data_publicacao TIMESTAMP NOT NULL default current_timestamp
+    data_publicacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-create table local (
+CREATE TABLE local (
     id SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(256) NOT NULL,
     website VARCHAR(50)
 );
 
-create table profissional (
+CREATE TABLE profissional (
     registro VARCHAR(128) NOT NULL PRIMARY KEY,
     especialidade VARCHAR(50) NOT NULL,
     email VARCHAR(128) NOT NULL
