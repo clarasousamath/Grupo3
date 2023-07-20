d43e3bc21d834c2232eab6269da75c4c0f894613
Mirraely <mirraelymoura@gmail.com>
2023-07-19 23:15:43 -0300

    Mapeia atributos compostos

--- docs/raizes_cura.sql
+++ docs/raizes_cura.sql
@@ -7,7 +7,9 @@ CREATE TABLE usuario (
     senha VARCHAR(256) NOT NULL,
     perfil VARCHAR(50) NOT NULL,
     bio TEXT NOT NULL,
-    data_cadastro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    nome VARCHAR(128) NOT NULL,
+    sobrenome VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE artigo (
@@ -19,13 +21,20 @@ CREATE TABLE artigo (
 CREATE TABLE local (
     id SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(256) NOT NULL,
-    website VARCHAR(50)
+    website VARCHAR(50),
+    logradouro VARCHAR(256) NOT NULL,
+    numero VARCHAR(20) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    uf VARCHAR(50) NOT NULL      
 );
 
 CREATE TABLE profissional (
     registro VARCHAR(128) NOT NULL PRIMARY KEY,
     especialidade VARCHAR(50) NOT NULL,
-    email VARCHAR(128) NOT NULL
+    email VARCHAR(128) NOT NULL,
+    nome VARCHAR(128) NOT NULL,
+    sobrenome VARCHAR(256) NOT NULL
 );
 
 
