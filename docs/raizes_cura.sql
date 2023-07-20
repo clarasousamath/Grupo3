db0b92ba2777da7cf855fd5b829b3f92217e722a
Mirraely <mirraelymoura@gmail.com>
2023-07-19 23:56:25 -0300

    Mapeia atributos multivalorados

--- docs/raizes_cura.sql
+++ docs/raizes_cura.sql
@@ -12,10 +12,28 @@ CREATE TABLE usuario (
     sobrenome VARCHAR(256) NOT NULL
 );
 
+CREATE TABLE usuario_telefone (
+    email_usuario VARCHAR(128) NOT NULL,
+    telefone VARCHAR(30) NOT NULL,
+    PRIMARY KEY (email_usuario, telefone),
+    CONSTRAINT fk_email_usuario
+        FOREIGN KEY (email_usuario)
+        REFERENCES usuario(email)
+);
+
 CREATE TABLE artigo (
     titulo VARCHAR(128) NOT NULL PRIMARY KEY,
     subtitulo VARCHAR(256),
     data_publicacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE artigo_conteudo (
+    titulo_artigo VARCHAR(128) NOT NULL,
+    conteudo TEXT,
+    PRIMARY KEY (titulo_artigo, conteudo),
+    CONSTRAINT fk_titulo_artigo
+        FOREIGN KEY (titulo_artigo)
+        REFERENCES artigo(titulo)
 );
 
 CREATE TABLE local (
@@ -29,12 +47,30 @@ CREATE TABLE local (
     uf VARCHAR(50) NOT NULL      
 );
 
+CREATE TABLE local_telefone (
+    id_local INT NOT NULL,
+    telefone VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id_local, telefone),
+    CONSTRAINT fk_id_local
+        FOREIGN KEY (id_local)
+        REFERENCES local(id)
+);
+
 CREATE TABLE profissional (
     registro VARCHAR(128) NOT NULL PRIMARY KEY,
     especialidade VARCHAR(50) NOT NULL,
     email VARCHAR(128) NOT NULL,
     nome VARCHAR(128) NOT NULL,
     sobrenome VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE profissional_telefone (
+    registro_profissional VARCHAR(128) NOT NULL,
+    telefone VARCHAR(30) NOT NULL,
+    PRIMARY KEY (registro_profissional, telefone),
+    CONSTRAINT fk_registro_profissional
+        FOREIGN KEY (registro_profissional)
+        REFERENCES profissional(registro)
 );
 
 
